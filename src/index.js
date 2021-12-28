ed6ccdb4d4b42d6884076dd01df4308b01da2b38
_j_7 <tang3@j-7s-MacBook-Air.local>
2021-12-28 00:31:52 -0800

    sandbox mutation request passes data input check

--- src/index.js
+++ src/index.js
@@ -1,90 +1,117 @@
 // from: https://www.apollographql.com/docs/apollo-server/getting-started/
 
-const { ApolloServer, gql } = require('apollo-server');
-const { MongoClient } = require('mongodb');
-const dotenv = require('dotenv');
+const { ApolloServer, gql } = require("apollo-server");
+const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const { DB_URI, DB_NAME } = process.env;    // pull from .env file
-
+const { DB_URI, DB_NAME } = process.env; // pull from .env file
 
 const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
 ];
-
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-
   type Query {
-      myTaskLists: [TaskList!]!
+    myTaskLists: [TaskList!]!
   }
+
+  type Mutation {
+    signUp(input: SignUpInput): AuthUser!
+    signIn(input: SignInInput): AuthUser!
+  }
+  
+  input SignUpInput {
+    email: String!
+    password: String!
+    name: String!
+    avatar: String
+  }
+
+  input SignInInput {
+    email: String!
+    password: String!
+  }
+
+  type AuthUser {
+    user: User!
+    token: String!
+  }
+
   type User {
-      id: ID!
-      name: String!
-      email: String!
-      avatar: String
+    id: ID!
+    name: String!
+    email: String!
+    avatar: String
   }
 
   type TaskList {
-      id: ID!
-      createdAt: String!
-      title: String!
-      progress: Float!
+    id: ID!
+    createdAt: String!
+    title: String!
+    progress: Float!
 
-      users: [User!]!
-      todos: [Todo!]!
+    users: [User!]!
+    todos: [Todo!]!
   }
 
   type Todo {
-      id: ID!
-      content: String!
-      isComplete: Boolean!
+    id: ID!
+    content: String!
+    isComplete: Boolean!
 
-      taskListId: ID!
-      taskList: TaskList!
+    taskListId: ID!
+    taskList: TaskList!
   }
 `;
-
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-    Query: {
-        myTaskLists: () => []
+  Query: {
+    myTaskLists: () => [],
+  },
+  Mutation: {
+    signUp: (_, data) => {
+      console.log(data.input.email);
+    },
+    signIn: () => {
+
     }
+  }
 };
 
 const start = async () => {
-    // MongoDB driver code. Connect to MongoDB
-    const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    await client.connect();
-    const db = client.db(DB_NAME);
+  // MongoDB driver code. Connect to MongoDB
+  const client = new MongoClient(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const db = client.db(DB_NAME);
 
-    const context = {
-        db,
-    }
+  const context = {
+    db,
+  };
 
-    // The ApolloServer constructor requires two parameters: your schema
-    // definition and your set of resolvers.
-    const server = new ApolloServer({ typeDefs, resolvers, context });
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({ typeDefs, resolvers, context });
 
-    // The `listen` method launches a web server.
-    server.listen().then(({ url }) => {
-        console.log(`🚀  Server ready at ${url}`);
-    });
-}
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+};
 
 start();
-
-
-
