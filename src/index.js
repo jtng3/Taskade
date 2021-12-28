d3f4203f52d2dc937efe85c983aabc35cdfed817
_j_7 <tang3@j-7s-MacBook-Air.local>
2021-12-27 23:39:48 -0800

    clean up (resolvers)

--- src/index.js
+++ src/index.js
@@ -59,11 +59,7 @@ const typeDefs = gql`
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-    Query: {
-        books: () => {
-            return books
-        },
-    },
+ 
 };
 
 const start = async () => {
