1d81def36d8031e61c7ab81a05e7a937fa65a3f6
_j_7 <tang3@j-7s-MacBook-Air.local>
2021-12-27 22:22:54 -0800

    mongo db connected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 // from: https://www.apollographql.com/docs/apollo-server/getting-started/
 
 const { ApolloServer, gql } = require('apollo-server');
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { DB_URI, DB_NAME } = process.env;    // pull from .env file
+
+
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -24,17 +42,6 @@ const typeDefs = gql`
 
 `;
 
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-];
-
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
@@ -44,12 +51,23 @@ const resolvers = {
     },
 };
 
+const start = async () => {
+    // MongoDB driver code. Connect to MongoDB
+    const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    const db = client.db(DB_NAME);
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+    // The ApolloServer constructor requires two parameters: your schema
+    // definition and your set of resolvers.
+    const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+    // The `listen` method launches a web server.
+    server.listen().then(({ url }) => {
+        console.log(`🚀  Server ready at ${url}`);
+    });
+}
+
+start();
+
+
+
